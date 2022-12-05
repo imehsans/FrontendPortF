2e4965d58e6f35d6c963b8d9968dfe96715b03ba
imehsans <mehsanjadoon786@gmail.com>
2022-12-05 12:34:13 -0500

    update: work components

--- myportfolio/src/components/work/Work.jsx
+++ myportfolio/src/components/work/Work.jsx
@@ -3,83 +3,224 @@ import React from 'react'
 const Work = () => {
 	var jobs = [
 		{
-			title: 'Frontend Developer1',
-			'company-name': 'ABC',
-			'joining-date': '1/1/2022',
-			'ends-Date': '1/1/2022',
+			title: 'Frontend Developer',
+			'company-name': 'Merik Solutions',
+			'joining-date': '24/06/2022',
+			'ends-Date': 'Current',
 			address: {
-				street: 'i-10/1',
-				city: 'abc',
-				country: 'xyz',
+				street: 'I-10/3',
+				city: 'Islamabad',
+				country: 'Pakistan',
+				link: 'https://meriksolutions.biz',
 			},
-			'working-time': '2:00PM to 12:00PM',
-			technologies: ['HTML', 'CSS', 'bootstrap'],
-			'company-type': 'It company',
-			desc: 'abc def ghi jkl',
+			'working-time': '6:00PM to 3:00AM',
+			technologies: [
+				'HTML',
+				'CSS',
+				'Bootstrap',
+				'Tailwind CSS',
+				'SASS',
+				'JavaScript',
+				'JQuery',
+				'AJAX',
+				'ReactJS',
+				'ReduxJS',
+				'GIT',
+			],
+			'company-type': 'IT Software Development',
+			desc: 'Working on different websites some of them from scratch and mostly working with live site and following XD and Figma designs and make all them with approximately Zero Error in UI.',
 		},
 		{
-			title: 'Frontend Developer2',
-			'company-name': 'ABC',
-			'joining-date': '1/1/2022',
-			'ends-Date': '1/1/2022',
+			title: 'Frontend Developer',
+			'company-name': 'Merik Solutions',
+			'joining-date': '24/06/2022',
+			'ends-Date': 'Current',
 			address: {
-				street: '',
-				city: 'abc',
-				country: 'xyz',
+				street: 'I-10/3',
+				city: 'Islamabad',
+				country: 'Pakistan',
+				link: 'https://meriksolutions.biz',
 			},
-			'working-time': '2:00PM to 12:00PM',
-			technologies: ['HTML', 'CSS', 'bootstrap'],
-			'company-type': 'It company',
-			desc: 'abc def ghi jkl',
+			'working-time': '6:00PM to 3:00AM',
+			technologies: [
+				'HTML',
+				'CSS',
+				'Bootstrap',
+				'Tailwind CSS',
+				'SASS',
+				'JavaScript',
+				'JQuery',
+				'AJAX',
+				'ReactJS',
+				'ReduxJS',
+				'GIT',
+			],
+			'company-type': 'IT Software Development',
+			desc: 'Working on different websites some of them from scratch and mostly working with live site and following XD and Figma designs and make all them with approximately Zero Error in UI.',
+		},
+		{
+			title: 'Frontend Developer',
+			'company-name': 'Merik Solutions',
+			'joining-date': '24/06/2022',
+			'ends-Date': 'Current',
+			address: {
+				street: 'I-10/3',
+				city: 'Islamabad',
+				country: 'Pakistan',
+				link: 'https://meriksolutions.biz',
+			},
+			'working-time': '6:00PM to 3:00AM',
+			technologies: [
+				'HTML',
+				'CSS',
+				'Bootstrap',
+				'Tailwind CSS',
+				'SASS',
+				'JavaScript',
+				'JQuery',
+				'AJAX',
+				'ReactJS',
+				'ReduxJS',
+				'GIT',
+			],
+			'company-type': 'IT Software Development',
+			desc: 'Working on different websites some of them from scratch and mostly working with live site and following XD and Figma designs and make all them with approximately Zero Error in UI.',
+		},
+		{
+			title: 'Frontend Developer',
+			'company-name': 'Merik Solutions',
+			'joining-date': '24/06/2022',
+			'ends-Date': 'Current',
+			address: {
+				street: 'I-10/3',
+				city: 'Islamabad',
+				country: 'Pakistan',
+				link: 'https://meriksolutions.biz',
+			},
+			'working-time': '6:00PM to 3:00AM',
+			technologies: [
+				'HTML',
+				'CSS',
+				'Bootstrap',
+				'Tailwind CSS',
+				'SASS',
+				'JavaScript',
+				'JQuery',
+				'AJAX',
+				'ReactJS',
+				'ReduxJS',
+				'GIT',
+			],
+			'company-type': 'IT Software Development',
+			desc: 'Working on different websites some of them from scratch and mostly working with live site and following XD and Figma designs and make all them with approximately Zero Error in UI.',
 		},
 	]
 
 	return (
-		<div className="container-flued bg-dark bg-opacity-10 text-dark md:px-0">
+		<div className="pb-8 container-flued bg-dark bg-opacity-10 text-dark md:px-0">
 			<div className="container px-1 py-4 mx-auto text-center md:px-0">
 				<h1 className="font-anton py-4 text-[24px] md:text-[30px] text-primary">
 					Work Experience
 				</h1>
-				<ul className="flex flex-col justify-between gap-10 md:flex-row">
+				<ul className="flex flex-col flex-wrap justify-between md:flex-row">
 					{jobs.map((job) => {
 						return (
 							<>
-								<li
-									className="w-full px-2 py-3 text-left bg-white shadow-xl max-auto rounded-tl-xl lg:w-1/2 rounded-br-xl md:px-4"
-									key={job.id}
-								>
-									<div className="">
-										<h1 className="text-[24px] font-normal">{job.title}</h1>
-										<div className="flex mb-4 items-center gap-x-3 text-[20px]">
-											<span className="italic font-semibold text-secondary">
-												{job['company-name']}
-											</span>
-											<span className="text-light">
-												[{job['joining-date']} - {job['ends-Date']} ]
-											</span>
-										</div>
-										<div>
-											<h1>Company Address</h1>
-											<div className="px-1">
-												{job.address.street.length !== 0 ? (
-													<p>Street : {job.address.street}</p>
-												) : (
-													''
-												)}
-												{job.address.city.length !== 0 ? (
-													<p>City : {job.address.city}</p>
-												) : (
-													''
-												)}
-												{job.address.country.length !== 0 ? (
-													<p>Street : {job.address.country}</p>
-												) : (
-													''
-												)}
+								{job ? (
+									<li
+										className="w-full px-2 py-3 text-left scale-95 bg-white shadow-xl hover:scale-100 max-auto md:w-1/2 rounded-xl md:px-4"
+										key={job?.id}
+									>
+										<div className="p-3">
+											<h1 className="text-[24px] font-normal">{job.title}</h1>
+											<div className="flex flex-wrap mb-3 items-center gap-x-3 text-[20px]">
+												<span className="italic font-semibold text-secondary">
+													{job['company-name']}
+												</span>
+												<span className="text-light">
+													[{job['joining-date']} - {job['ends-Date']} ]
+												</span>
 											</div>
+											{job.address ? (
+												<div className="mb-3">
+													<h1>Company Address</h1>
+													<div>
+														{job?.address?.street ? (
+															<p>-Street : {job?.address?.street}</p>
+														) : (
+															''
+														)}
+														{job?.address?.city ? (
+															<p>-City : {job?.address?.city}</p>
+														) : (
+															''
+														)}
+														{job?.address?.country ? (
+															<p>-Street : {job?.address?.country}</p>
+														) : (
+															''
+														)}
+														{job?.address?.link ? (
+															<div>
+																<a
+																	href={job?.address?.link}
+																	target="_blank"
+																	className="underline text-secondary"
+																	rel="noreferrer"
+																>
+																	View Company Website
+																</a>
+															</div>
+														) : (
+															''
+														)}
+													</div>
+												</div>
+											) : (
+												''
+											)}
+											{job['working-time'] ? (
+												<div className="mb-3">
+													<p>Working Time : {job?.['working-time']}</p>
+												</div>
+											) : (
+												''
+											)}
+											{job?.desc ? (
+												<div className="mb-3">
+													<h1>Description:</h1>
+													<p>{job?.desc}</p>
+												</div>
+											) : (
+												''
+											)}
+											{job?.technologies ? (
+												<div className="mb-3">
+													<h1 className="mb-2">
+														Works On The Following Technologies:
+													</h1>
+													<ul className="flex flex-wrap py-3 gap-x-3 gap-y-6">
+														{job.technologies.map((tech) => {
+															return (
+																<li key={tech?.id}>
+																	<label className="px-3 py-2 capitalize border rounded border-primary text-primary hover:text-secondary hover:border-secondary">
+																		{tech}
+																	</label>
+																</li>
+															)
+														})}
+													</ul>
+												</div>
+											) : (
+												''
+											)}
 										</div>
-									</div>
-								</li>
+									</li>
+								) : (
+									<li>
+										<p className="p-4 bg-white text-dark">Zero Experience!</p>
+									</li>
+								)}
 							</>
 						)
 					})}
